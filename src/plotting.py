39751e7ab01efaf00073c4a3eadaeb649537222e
Noah <noah@vgrinsven.com>
2021-01-22 17:48:00 +0100

    Change plots save dir. Plots are now saved together with other results

--- src/plotting.py
+++ src/plotting.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 #import gif
 
 """
@@ -61,7 +62,7 @@ def plotExplanation(Xhats, yhats, save_path=None):
                 axs[isamp,iz].axis('off')
         axs[0,round(nz_sweep/2)-1].set_title('Sweep latent dimension %d' % (latent_dim+1))
         if save_path is not None:
-            plt.savefig('./%s_latentdim%d.svg' % (save_path,latent_dim+1), bbox_inches=0)
+            plt.savefig(os.path.join(save_path, 'latentdim{}.svg'.format(latent_dim+1)), bbox_inches=0)
 
 
 def outline_mask(ax, mask, bounds=(0,1,0,1), color=(0,0,0,0.25)):
