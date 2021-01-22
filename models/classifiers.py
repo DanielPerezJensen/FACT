4b8708d4a592d89d1815ac2b75a9366e6561cb4f
Daniel Perez Jensen <danielperezjensen@gmail.com>
2021-01-22 16:34:33 +0100

    Added ResNet and DenseNet derivatives

--- models/classifiers.py
+++ models/classifiers.py
@@ -2,11 +2,15 @@
 
 """Classes that define our self-created classifier models to aid in
 the reproducibility study of
-'Generative causal explanations of black-box classifiers'"""
+'Generative causal explanations of black-box classifiers'
+
+Retrieved from: https://uvadlc-notebooks.readthedocs.io/en/latest/tutorial_notebooks/tutorial5/Inception_ResNet_DenseNet.html
+"""
 
 import os
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 
 class InceptionBlock(nn.Module):
@@ -97,14 +101,15 @@ class InceptionNetDerivative(nn.Module):
         x = self.input_net(x)
         x = self.blocks(x)
         y = self.output_net(x)
+        prob = F.softmax(y, dim=-1)
 
-        return y
+        return prob, y
 
 
 class ResNetBlock(nn.Module):
     """Original ResNetBlock, described in https://arxiv.org/abs/1603.05027"""
 
-    def __init__(self, c_int, act_fn, c_out):
+    def __init__(self, c_in, act_fn, c_out):
         """
         Inputs:
             c_in - Number of input dims
@@ -114,21 +119,177 @@ class ResNetBlock(nn.Module):
         super().__init__()
 
         self.net = nn.Sequential(
-                nn.Conv2d(c_in, c_out, kernel_size=3, padding=1, stride=2, bias=False),
+                nn.Conv2d(c_in, c_out, kernel_size=3, padding=1, stride=1, bias=False),
                 nn.BatchNorm2d(c_out),
                 act_fn(),
                 nn.Conv2d(c_out, c_out, kernel_size=3, padding=1, bias=False),
                 nn.BatchNorm2d(c_out)
             )
-        self.downsample = nn.Conv2d(c_in, c_out, kernel_size=1, stride=2)
 
         self.act_fn = act_fn()
 
     def forward(self, x):
         z = self.net(x)
-        x = self.downsample(x)
 
         out = z + x
         out = self.act_fn(out)
 
         return out
+
+
+class ResNetDerivative(nn.Module):
+    """Derivative of ResNet with only one group of blocks"""
+
+    def __init__(self, num_classes=10, act_fn=nn.ReLU):
+        super().__init__()
+
+        self.num_classes = num_classes
+        self.act_fn = act_fn
+
+        self.input_net = nn.Sequential(
+                nn.Conv2d(1, 16, kernel_size=3, padding=1, bias=False),
+                nn.BatchNorm2d(16),
+                self.act_fn()
+            )
+
+        self.blocks = nn.Sequential(
+                ResNetBlock(16, self.act_fn, 16),
+                ResNetBlock(16, self.act_fn, 16),
+                ResNetBlock(16, self.act_fn, 16)
+            )
+
+        self.output_net = nn.Sequential(
+                nn.AdaptiveAvgPool2d((1, 1)),
+                nn.Flatten(),
+                nn.Linear(16, self.num_classes)
+            )
+
+    def forward(self, x):
+        x = self.input_net(x)
+        x = self.blocks(x)
+        y = self.output_net(x)
+        prob = F.softmax(y, dim=-1)
+
+        return y, prob
+
+
+class DenseLayer(nn.Module):
+    """Dense layer from DenseNet, described in https://arxiv.org/abs/1608.06993"""
+
+    def __init__(self, c_in, bn_size, growth_rate, act_fn):
+        """
+        Inputs:
+            c_in - Number of input channels
+            bn_size - Bottleneck size (factor of growth rate) for the output of the 1x1 convolution. Typically between 2 and 4.
+            growth_rate - Number of output channels of the 3x3 convolution
+            act_fn - Activation class constructor (e.g. nn.ReLU)
+        """
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.BatchNorm2d(c_in),
+            act_fn(),
+            nn.Conv2d(c_in, bn_size * growth_rate, kernel_size=1, bias=False),
+            nn.BatchNorm2d(bn_size * growth_rate),
+            act_fn(),
+            nn.Conv2d(bn_size * growth_rate, growth_rate, kernel_size=3, padding=1, bias=False)
+        )
+
+    def forward(self, x):
+        out = self.net(x)
+        out = torch.cat([out, x], dim=1)
+        return out
+
+
+class DenseBlock(nn.Module):
+    """Dense Block from DenseNet, described in https://arxiv.org/abs/1608.06993"""
+
+    def __init__(self, c_in, num_layers, bn_size, growth_rate, act_fn=nn.ReLU):
+        """
+        Inputs:
+            c_in - Number of input channels
+            num_layers - Number of dense layers to apply in the block
+            bn_size - Bottleneck size to use in the dense layers
+            growth_rate - Growth rate to use in the dense layers
+            act_fn - Activation function to use in the dense layers
+        """
+        super().__init__()
+        layers = []
+
+        for layer_idx in range(num_layers):
+            layers.append(DenseLayer(c_in=c_in + layer_idx * growth_rate,
+                                     bn_size=bn_size,
+                                     growth_rate=growth_rate,
+                                     act_fn=act_fn))
+
+        self.block = nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = self.block(x)
+        return out
+
+
+class TransitionLayer(nn.Module):
+    """Transition Layer from DenseNet, described in https://arxiv.org/abs/1608.06993"""
+
+    def __init__(self, c_in, c_out, act_fn):
+        super().__init__()
+        self.transition = nn.Sequential(
+            nn.BatchNorm2d(c_in),
+            act_fn(),
+            nn.Conv2d(c_in, c_out, kernel_size=1, bias=False),
+            nn.AvgPool2d(kernel_size=2, stride=2)
+        )
+
+    def forward(self, x):
+        return self.transition(x)
+
+
+class DenseNetDerivative(nn.Module):
+    """Derivative of ResNet with only one group of blocks"""
+    def __init__(self, num_classes=2, num_layers=[3, 3], bn_size=2, growth_rate=16, act_fn=nn.ReLU):
+        super().__init__()
+
+        c_hidden = growth_rate * bn_size
+
+        self.input_net = nn.Sequential(
+                nn.Conv2d(1, c_hidden, kernel_size=3, padding=1)
+            )
+
+        # Creating dense block, including transition layers for the last block
+        blocks = []
+        for block_idx, num_layer in enumerate(num_layers):
+            blocks.append(
+                DenseBlock(c_in=c_hidden,
+                           num_layers=num_layer,
+                           bn_size=bn_size,
+                           growth_rate=growth_rate,
+                           act_fn=act_fn)
+                )
+            c_hidden = c_hidden + num_layer * growth_rate
+
+            if block_idx < len(num_layers) - 1:
+                blocks.append(
+                    TransitionLayer(c_in=c_hidden,
+                                    c_out=c_hidden // 2,
+                                    act_fn=act_fn)
+                    )
+                c_hidden = c_hidden // 2
+
+        self.blocks = nn.Sequential(*blocks)
+
+        self.output_net = nn.Sequential(
+            nn.BatchNorm2d(c_hidden),
+            act_fn()
+            ,
+            nn.AdaptiveAvgPool2d((1, 1)),
+            nn.Flatten(),
+            nn.Linear(c_hidden, num_classes)
+        )
+
+    def forward(self, x):
+        x = self.input_net(x)
+        x = self.blocks(x)
+        out = self.output_net(x)
+        prob = F.softmax(out, dim=-1)
+
+        return prob, out
--- train_classifier.py
+++ train_classifier.py
@@ -12,6 +12,8 @@ import time
 import torch
 import torch.nn as nn
 from torch.utils.tensorboard import SummaryWriter
+from torch.optim.lr_scheduler import StepLR
+
 
 import datetime
 
@@ -46,11 +48,11 @@ def main():
 
     save_folder_root = './models/classifiers/'
     save_folder = os.path.join(save_folder_root,
-                               f"{model_name}_{dataset}_{class_use_str}_classifier")
-    summary_writer = SummaryWriter(save_folder + "/checkpoints")
+                               f"{model_name.lower()}_{dataset}_{class_use_str}_classifier")
+    summary_writer = SummaryWriter(save_folder + "/runs")
 
     os.makedirs(save_folder, exist_ok=True)
-    os.makedirs(save_folder + "/checkpoints", exist_ok=True)
+    os.makedirs(save_folder + "/runs", exist_ok=True)
 
     # Dataset preparation
     if dataset == 'mnist':
@@ -71,13 +73,31 @@ def main():
     # Training
     ce_loss = nn.CrossEntropyLoss()
 
-    classifier = classifiers.InceptionNetDerivative(num_classes=y_dim).to(device)
-    optimizer = torch.optim.SGD(classifier.parameters(), lr=lr, momentum=momentum)
+    # Import stated model
+    if model_name.lower() == "inceptionnet":
+        classifier = classifiers.InceptionNetDerivative(num_classes=y_dim).to(device)
+    elif model_name.lower() == "resnet":
+        classifier = classifiers.ResNetDerivative(num_classes=y_dim).to(device)
+    elif model_name.lower() == "densenet":
+        classifier = classifiers.DenseNetDerivative(num_classes=y_dim).to(device)
+    else:
+        raise InputError("Invalid model_name, options=['InceptionNet', 'ResNet', 'DenseNet']")
 
-    print(classifier)
+    optimizer = torch.optim.SGD(classifier.parameters(), lr=lr, momentum=momentum)
+    scheduler = StepLR(optimizer, step_size=1, gamma=0.7)
+
+    best_loss = np.inf
 
     for epoch in range(epochs):
+
+        total_train_loss = 0.0
+        total_train_acc = 0
+
+        total_test_loss = 0.0
+        total_test_acc = 0
+
         for idx in range(batch_idxs):
+            classifier.train()
             # Gather input and labels for current batch
             labels = torch.from_numpy(trY[idx * batch_size:(idx + 1) * batch_size]).long().to(device)
             inp = trX[idx * batch_size:(idx + 1) * batch_size]
@@ -87,27 +107,75 @@ def main():
 
             # Calculate loss and acc, then step with optimizer
             optimizer.zero_grad()
-            probs = classifier(inp)
-            loss = ce_loss(probs, labels)
+            probs, out = classifier(inp)
+            loss = ce_loss(out, labels)
             loss.backward()
 
             acc = (probs.argmax(dim=-1) == labels).float().mean()
             optimizer.step()
 
-            print("[{}] Train Epoch {:03d}/{:03d}, Batch Size = {}, \
-                  loss = {:.3f}, acc = {}".format(
+            # Add loss and accuracy to acumulators
+            total_train_loss += loss.item()
+            total_train_acc += acc
+
+            print("[{}] Train Epoch {:02d}/{:02d}, Batch Size = {}, \
+                  train loss = {:.3f}, train acc = {:.2f}".format(
                     datetime.datetime.now().strftime("%Y-%m-%d %H:%M"), epoch,
                     epochs, batch_size, loss.item(), acc))
+
+        total_train_loss /= batch_idxs
+        total_train_acc /= batch_idxs
+
+        summary_writer.add_scalar("Loss/train", total_train_loss, epoch)
+        summary_writer.add_scalar("Acc/train", total_train_acc, epoch)
+
+        for idx in range(batch_idxs_val):
+            classifier.eval()
+            # Gather validation input and labels
+            val_labels = torch.from_numpy(vaY[idx * test_size:(idx + 1) * test_size]).long().to(device)
+            val_inp = vaX[idx * test_size:(idx + 1) * test_size]
+            val_inp = torch.from_numpy(val_inp)
+            val_inp = val_inp.permute(0, 3, 1, 2).float()
+            val_inp = val_inp.to(device)
+
+            probs, out = classifier(val_inp)
+            total_test_loss += ce_loss(out, val_labels)
+            total_test_acc += (probs.argmax(dim=-1) == val_labels).float().mean()
+
+        total_test_loss /= batch_idxs_val
+        total_test_acc /= batch_idxs_val
+
+        summary_writer.add_scalar("Loss/val", total_test_loss, epoch)
+        summary_writer.add_scalar("Acc/val", total_test_acc, epoch)
+
+        if total_test_loss < best_loss:
+            print(f"New best model, saving to: {save_folder}")
+
+            torch.save({
+                    "epoch": epoch,
+                    "model_state_dict_classifier": classifier.state_dict(),
+                    "optimizer_state_dict": optimizer.state_dict(),
+                }, os.path.join(save_folder, "model.pt"))
+
+        print("[{}] Test Epoch {:02d}/{:02d}, \
+              test loss = {:.3f}, test acc = {:.2f}".format(
+                datetime.datetime.now().strftime("%Y-%m-%d %H:%M"), epoch,
+                epochs, total_test_loss, total_test_acc))
+
+        scheduler.step()
+
+    summary_writer.flush()
+    summary_writer.close()
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--model", type=str, default="inception",
+    parser.add_argument("--model", type=str, default="InceptionNet",
                         help="Specification of model to be trained.")
     parser.add_argument("--dataset", type=str, default="mnist",
                         help="Specification of dataset to be used.")
-    parser.add_argument("--class_use", type=int, default=[3, 8],
+    parser.add_argument("--class_use", type=int, default=[3, 8], nargs="+",
                         help="Specification of what classes to use" +
                              "To specify multiple, use \" \" to" +
                              "separate them. Example \"3 8\".")
